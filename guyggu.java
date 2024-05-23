14eb7b99c0dc344398c048bb10595e0b60692483
niu <niu@test.com>
2024-05-24 00:31:09 +0800

    change

--- guyggu.java
+++ guyggu.java
@@ -1,17 +1,8 @@
-import java.util.LinkedList;
-
 public class guyggu {
 
     public static void main(String[] args) {
         System.out.println("hello world");
-        LinkedList<Integer> list = new LinkedList<>();
-        list.add(1);
-        list.add(2);
-        list.add(3);
-        list.add(4);
-        list.add(5);
-        list.offer(6);
-        System.out.println(list.poll());
+
     }
 
 }
